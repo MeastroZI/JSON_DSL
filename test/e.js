188f1897a30a7d37d97f8928228733a0e20a4a22
MeastroZI <vinitsharma2480@gmail.com>
2024-03-05 20:26:27 +0530

    initial commit version -0.0.1

---
+++ test/e.js
@@ -0,0 +1,60 @@
+
+
+const {Convert} = require("../lib/DSL")
+
+
+const transformRule = [
+
+{
+    path: '*',
+    conditions: [{ "isKey": "items", "hasSibling": ["type", "array"] }],
+    operations: {
+        "editKey": "prefixItems"
+    }
+} , 
+{
+    path : '$schema' ,
+  
+    operations : {
+        "updateValue" : "https://json-schema.org/draft/2020-12/schema"
+    }
+}
+]
+
+
+
+const jasonobj = {
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
+    "type": "object",
+    "properties": {
+        "items": {
+            "type": "array",
+            "items": [
+                { "type": "string" }
+            ]
+        },
+        "extra": {
+            "$ref": "#/properties/items/items/0"
+        }
+    },
+    "ooos": {
+        "items2": {
+            "type": "array",
+            "items": []
+        },
+        "item3": {
+            "items4": {
+                "items5": {
+                    "type": "array",
+                    "items": []
+                }
+            }
+        }
+    }
+}
+
+const myObj = Convert(transformRule , jasonobj)
+
+console.log(JSON.stringify(myObj , null , 1))
+
+
